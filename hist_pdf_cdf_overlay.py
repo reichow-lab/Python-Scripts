2e914317c9f8097a165bd53bf9415fabb9e285e4
Matthew Veter <veter@pdx.edu>
2019-08-01 14:37:47 -0700

    Now User Friendly!

--- hist_pdf_cdf_overlay.py
+++ hist_pdf_cdf_overlay.py
@@ -16,6 +16,10 @@ from sys import argv
 
 script, infile	=	argv
 
+x_lab	=	input("Name the x-axis: ")
+num_bins=	input("How many bins? ")
+pdf_col	=	input("Color of the pdf? ")
+
 # Unloading data file
 x, y	=	np.loadtxt(infile, delimiter='\t', unpack=True)
 
@@ -24,7 +28,7 @@ fig1	=	figure()
 
 # Creating the pdf subplot
 ax1	=	fig1.add_subplot(111)
-n, bins, patches	=	ax1.hist(y, bins='auto', density=1, facecolor='aqua', alpha=0.5)
+n, bins, patches	=	ax1.hist(y, num_bins, density=1, facecolor=pdf_col, alpha=0.5)
 
 # Establishing the y-axis of the pdf and the x-axis
 ylabel("Probability density")
@@ -34,16 +38,16 @@ plt.xticks(np.arange(-180,181, step=45))
 
 # Overlaying the line of the pdf to give the edge definition
 ax2	=	fig1.add_subplot(111)
-n, bins, patches	=	ax2.hist(y, bins='auto', density=1, histtype='step', color='black', alpha=0.5)
+n, bins, patches	=	ax2.hist(y, num_bins, density=1, histtype='step', color='black', alpha=0.5)
 
 # Creating the cdf subplot
 ax3	=	fig1.add_subplot(111, sharex=ax1, frameon=False)
-ax3.hist(y, bins='auto', density=1, color='navy', histtype='step', cumulative=True)
+ax3.hist(y, num_bins, density=1, color='navy', histtype='step', cumulative=True)
 
 # Establishing the y-axis of the cdf
 ax3.yaxis.tick_right()
 ax3.yaxis.set_label_position("right")
-xlabel("Dihedral angle (degrees)")
+xlabel(x_lab)
 ylabel("Cumulative density")
 
 # Creating shared traits and the figure
