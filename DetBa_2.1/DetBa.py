73386c08c49572f435e93447bf963ff066eed457
Matthew Veter <veter@pdx.edu>
2020-01-31 13:32:16 -0800

    Using default DiffC

--- DetBa_2.1/DetBa.py
+++ DetBa_2.1/DetBa.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 #
 #	Program: DetBa.py
-#	
-#	The purpose of this program is to replace the current version DetBa_2.sh, a bash script that strung together a series of python scripts in an 
+#
+#	The purpose of this program is to replace the current version DetBa_2.sh, a bash script that strung together a series of python scripts in an
 #	ad-hoc fashion that was clunky, and potentially prone to error. This version will be entirely contained within this python program that uses the
 #	aforementioned analysis scripts as modules.
 #
-#	initializer.py and continuator.py will be replaced by a singel program that does both jobs, since I can write a python conditional that can distinguish 
+#	initializer.py and continuator.py will be replaced by a singel program that does both jobs, since I can write a python conditional that can distinguish
 #	between the first and not-first ion trajectories. This means that I will have to go into thos scripts (more likely create a new one) that creates functions that
-#	can be called by this (DetBa.py) program. 
+#	can be called by this (DetBa.py) program.
 #
 #	This program should also make the original (DetBa.sh) script completely obsolete! The original script had a primitive binning mechanism that only allowed for rigid
 #	1 A sized bins, which was re-done in DetBa_2 to allow for any bin size including sub angstrom bins, which proved useful calculating histograms for interatomic distances
@@ -17,11 +17,11 @@
 #
 #	Additions
 #	_________
-#	
+#
 #	- Histogram_Builder	(1) -- working to make output in bins, and not angstroms. Or, if I am going to have angstroms, they should be accurate to the coarsed-graining.
-#		
+#
 #		- This option will allow one to create histograms of their data without creating excess rates based files
-#		  making the program 'Histobot' obsolete. 
+#		  making the program 'Histobot' obsolete.
 #		- Furthermore this will be the method used for creating distance based histograms. (see Myers, J.B. & Haddad, B.G. (Nature, 2018))
 #		- Currently working on this, and almost finished, refer to README: 27-Sep-2018
 #
@@ -42,15 +42,15 @@
 #
 #	- Current_Calculator	(1)
 #
-#		- This module will use the formulation shown in Zonta et al. 2014, to calculate the mean first passage time (MFPT) of an ion traversing the PMF. Eventually this module will calculate the current given a voltage potential, and therefore an approximated I-V Curve.  
+#		- This module will use the formulation shown in Zonta et al. 2014, to calculate the mean first passage time (MFPT) of an ion traversing the PMF. Eventually this module will calculate the current given a voltage potential, and therefore an approximated I-V Curve.
 #
 #		- It will take a PMF, and Diffusion coefficient (which will eventually be calculated in this module), then use those to calculate the MFPT in either direction of the PMF... the current can be calculated as:
 #
 #			I = q(K_lr - K_rl)
 #
-#		Such that I is current, q is the charge of the particle and K is the rate constant calculated as (1/MFPT), the lr & rl subscripts denote left-right, and right-left. 
+#		Such that I is current, q is the charge of the particle and K is the rate constant calculated as (1/MFPT), the lr & rl subscripts denote left-right, and right-left.
 #
-#		
+#
 #
 #
 #
@@ -164,7 +164,7 @@ while END == False:
 
 		Diff_pore		=	Diff_Calc(trans_mat,bin_size)						# Calculates in units (A^2/second)
 
-		print	"Diff_pore = %s" % Diff_pore
+		#print	"Diff_pore = %s" % Diff_pore
 
 		Voltages		=	[-150,-100,-75,-50,-25,0,25,50,75,100,150]
 
@@ -178,7 +178,7 @@ while END == False:
 
 			pmf_v		=	VoltPMF(PMF_0,dV,num_bins)
 
-			I,Tau_f,Tau_r	=	Current(pmf_v,num_bins,bin_size,Diff_pore)
+			I,Tau_f,Tau_r	=	Current(pmf_v,num_bins,bin_size)
 
 			out.write(str(dV) + ',' + str(I) + ',' + str(Tau_f) + ',' + str(Tau_r) + '\n')
 
