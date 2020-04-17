a08b0dd5d3e78d9bac10571e0ef0660de1910298
Matthew Veter <veter@pdx.edu>
2020-04-16 20:32:29 -0700

    APBS_Analysis finished

--- APBS_Analysis.py
+++ APBS_Analysis.py
@@ -18,7 +18,7 @@ Volume_List = []
 
 for file in volume_file_list:
 
-	Volume_List.append(gd.Grid(file))
+        Volume_List.append(gd.Grid(file))
 
 # Find center (x,y) of array (i.e. center of the Gap Junction pore – aligned using VMD)
 
@@ -27,10 +27,10 @@ Yo_list = []
 
 for den in Volume_List:
 
-	lenx, leny, lenz = den.grid.shape
+        lenx, leny, lenz = den.grid.shape
 
-	Xo_list.append(int(lenx/2))
-	Yo_list.append(int(leny/2))
+        Xo_list.append(int(lenx/2))
+        Yo_list.append(int(leny/2))
 
 # Find list of all (x,y) pairs that are within core of radius = rmin
 
@@ -40,18 +40,17 @@ ymax = int(rmin)
 x_list = []
 y_list = []
 for xo,yo in zip(Xo_list,Yo_list):
-	x_list_temp = []
-	y_list_temp = []
-	for x in range(0,xmax + 1,1):
-		for y in range(0,ymax + 1,1):
-			if (((x)**2)+((y)**2)) <= (rmin)**2: # This is the equation for all circles with a radii <= rmin
-				x_list_temp.append(xo + x)
-				y_list_temp.append(yo + y)
-			else:
-				pass
+        x_list_temp = []
+        y_list_temp = []
+        for x in range(0,xmax + 1,1):
+                for y in range(0,ymax + 1,1):
+                        if (((x)**2)+((y)**2)) <= (rmin)**2: # This is the equation for all circles with a radii <= rmin
+                                x_list_temp.append(xo + x)
+                                y_list_temp.append(yo + y)
+                        else:
+                                pass
         x_list.append(x_list_temp)
         y_list.append(y_list_temp)
-        print(len(x_list),len(y_list))
 # Average the potentials from each point in the circular plane for a given z-value
 
 CenterPots = []
@@ -59,27 +58,27 @@ Pore_Axes  = []
 denN       = 0
 for den in Volume_List:
 
-	x,y,z     = den.edges
-	Pore_Axes.append(z[0:-1])
-	PotentialTemp = []
-	Avg_pot   = 0
-	total_pot = 0
+        x,y,z     = den.edges
+        Pore_Axes.append(z[0:-1])
+        PotentialTemp = []
+        Avg_pot   = 0
+        total_pot = 0
+        for z in range(0,lenz,1):
+                for i in range(0,len(x_list[denN]),1):
+                        point_pot = den.grid[x_list[denN][i]][y_list[denN][i]][z]
+                        total_pot = total_pot + point_pot
 
-	for i in range(0,len(x_list[denN]),1):
-		point_pot = den.grid[x_list[denN][i]][y_list[denN][i]][z]
-		total_pot = total_pot + point_pot
-
-	Avg_pot = total_pot / len(x_list[denN])
-	PotentialTemp.append(Avg_pot)
-	CenterPots.append(PotentialTemp)
-	denN += 1
+                Avg_pot = total_pot / len(x_list[denN])
+                PotentialTemp.append(Avg_pot)
+        CenterPots.append(PotentialTemp)
+        denN += 1
 
 # Save Extracted data for future processing
 
 with open(str(globstring + '_data.pkl'), 'wb') as out:
 
-	pkl.dump(CenterPots, out)
-	pkl.dump(Pore_Axes, out)
+        pkl.dump(CenterPots, out)
+        pkl.dump(Pore_Axes, out)
 
 # Calculate mean and error
 
