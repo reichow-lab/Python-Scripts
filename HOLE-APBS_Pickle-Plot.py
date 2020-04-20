30ad387344a7c84d9411a38a8b35072a2929595d
Bassam Haddad <bhaddad@pdx.edu>
2020-04-20 14:39:43 -0700

    fix

--- HOLE-APBS_Pickle-Plot.py
+++ HOLE-APBS_Pickle-Plot.py
@@ -35,7 +35,7 @@ with open(exp, 'rb') as datain:
 with open((outname + '.txt'), 'w') as excelout:
 	excelout.write('Pore-Axis\tExp\tMD\t+SEM\t-SEM\n')
 	for i in range(0,len(Pore_Axes),1):
-		excelout.write(str(Pore_Axes[i])+'\t'+str(experiment[i])+str(Final[0][i])+'\t'+str(Final[1][i])+'\t'+str(Final[2][i])+'\n')
+		excelout.write(str(Pore_Axes[i])+'\t'+str(experiment[0][i])+str(Final[0][i])+'\t'+str(Final[1][i])+'\t'+str(Final[2][i])+'\n')
 	excelout.close()
 # Plot Data
 
