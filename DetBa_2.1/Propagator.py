b832f4df43430b3942b380b461f237cc1eafdbd1
Bassam Haddad <bhaddad@pdx.edu>
2020-05-19 23:17:47 -0700

    fix

--- DetBa_2.1/Propagator.py
+++ DetBa_2.1/Propagator.py
@@ -24,7 +24,7 @@ import itertools as it
 # Initializes pop_mat for subsequent calculations. MxN pop_mat, such that M is number of bins (num_bins) long, and either 2 or num_bins wide. The N=2 pop_mat is for simple histograms, and the N=num_bins pop_mat is for transition matrices.
 # Additionally, initialize() creates a dictionary where the keys are the int(zcoord) and the values are the bin indeces.
 
-def initialize(file_list, bin_size, outname, array_dim, bin_lim='auto':
+def initialize(file_list, bin_size, outname, array_dim, bin_lim='auto'):
     bin_min = 0
     bin_max = 0
     if bin_lim == 'auto':
