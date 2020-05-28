60dbb35e5e962884544a8eb3a50ffe54fe409f6b
Bassam Haddad <bhaddad@pdx.edu>
2020-05-28 16:59:18 -0700

    fix

--- DetBa_2.1/DetBa.py
+++ DetBa_2.1/DetBa.py
@@ -106,10 +106,13 @@ while END == False:
         rate_matrix =    pop2rate(num_bins, pop_matrix)
         rate_matrix.dump(out_rate_mat)
         gibbs       =   rate2gibbs(num_bins, bin_min, rate_matrix, bin_size, str(outname + '_rate'))
+        Prep(gibbs, str(outname + '_rate_final.txt'))
         source      = int(input("Which bin is the source? "))
         sink        = int(input("which bin is the sink? "))
         gibbs,K_AB,MFPT =   mfpt(pop_matrix,num_bins,outname,source,sink,bin_min,bin_max,bin_size,ZtoBin,lag_time)
-        Final       =   Prep(gibbs, out_final)
+        Prep(gibbs, out_final)
+
+
 
     elif choice == 'H':
         d_col        =    int(input("Which column from your data_file will you use? "))
