59e83a98b84cf417ca0c6a1e1a59e04731a1cf2f
Bassam Haddad <ghaddad.bassam@gmail.com>
2020-09-10 18:35:42 -0700

    fixed asym to average out the ends

--- DetBa_2.1/PMF_Prep.py
+++ DetBa_2.1/PMF_Prep.py
@@ -59,7 +59,7 @@ def final(PMF_avg):
         PMF_fin = [[],[]]
         PMF_fin[0] = PMF_avg[0][:]
         for i in range(len(PMF_avg[0])):
-            PMF_fin[1].append(PMF_avg[1][i] - PMF_avg[1][0])
+            PMF_fin[1].append(PMF_avg[1][i] - (((PMF_avg[1][0])+(PMF_ang[1][-1]))/2))
         return PMF_fin
 #################################################################
 def interp(PMF_in):
