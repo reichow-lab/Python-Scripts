bc8b4e09c2795b7fafd1d4cc3cef76cd6fc12412
Bassam Haddad <bhaddad@pdx.edu>
2020-09-10 20:22:23 -0700

    fixed

--- BlockMD.py
+++ BlockMD.py
@@ -31,6 +31,8 @@ def Block2D(M, InArray, Pore):
     AvgList = []
     for i in range(int(M)):
         AvgList.append(np.mean(bList[i], axis=0))
+    if M == 4:
+        with open((args.outname+"_"+str(M)+"_final.txt"))
     Final = [[],[]]
     Final[0].append(np.mean(AvgList, axis=0))
     Final[1].append(np.std(AvgList, axis=0)/np.sqrt(M))
