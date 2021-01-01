4cd49f1ca5dd773912ba8eaf9d3a57ad1fcc4e72
Bassam Haddad <ghaddad.bassam@gmail.com>
2020-12-31 16:48:02 -0800

    git

--- DetBa_2.1/Current_Calculator.py
+++ DetBa_2.1/Current_Calculator.py
@@ -46,77 +46,40 @@ R	= 0.00198588		# Ideal gas constant in units Kcal/molK
 T	= 310			# Temperature in units K
 
 def Current(PMF,num_bins,bin_s,DifC=1.957e11,q=1.60217663e-7):
-
 	tau_forward = 0
 	tau_reverse = 0
-
 	for y in range(0,num_bins,1):
-
 		for z in range(0,y+1,1):
-
 			hold		= np.exp((PMF[y,1] - PMF[z,1])/(R*T))*(bin_s**2)
-
 			tau_forward	= tau_forward + hold
-
 	for y in range(num_bins-1,-1,-1):
-
 		for z in range(num_bins-1,y-1,-1):
-
 			hold		= np.exp((PMF[y,1] - PMF[z,1])/(R*T))*(bin_s**2)
-
 			tau_reverse	= tau_reverse + hold
-
 	tau_forward	= tau_forward/DifC
-
 	tau_reverse	= tau_reverse/DifC
-
 	K_forward	= 1/tau_forward
-
 	K_reverse	= 1/tau_reverse
-
 	Current		= q*(K_forward - K_reverse)
-
 	return (Current,tau_forward,tau_reverse)
-
 def Text2PMF(PMF,n=0):
-
 	num_bins	= sum(1 for line in open(PMF))
 	PMF_z		= np.zeros((num_bins,2))
-
 	pmfile		= open(PMF)
-
 	for dataline in pmfile:
-
 		val		= dataline.split()
-
 		PMF_z[n,1]	= float(val[1])
-
 		PMF_z[n,0]	= float(val[0])
-
 		n		+= 1
-
 	return (PMF_z,num_bins)
-
 def VoltPMF(PMF,dV,num_bins,ion_sign=1,q=1.60217663e-7):
-
 	if ion_sign == 1:
-
 		q	= q
-
 	elif ion_sign == -1:
-
 		q	= -q
-
 	U_x	= np.zeros((num_bins,2))	# Initialize new voltage applied PMF array
-
 	dV_e	= float(dV) * (0.001) * 23.061	# Conversion of mV (dV) to Kcal/mol (dV_e)
-
 	for x in range(0,num_bins,1):
-
 		U_x[x,1]	= PMF[x,1] + ((dV_e)/(num_bins))*(num_bins - x)
-
 		U_x[x,0]	= PMF[x,0]
-
 	return U_x
-
-###################################################################################################
