c96e59eb5a028b79dd9cf279ff881d246f5edde9
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-01-03 23:58:57 -0800

    fixed Ion_Tracker.py to accept new ion_trajectory formats.

--- DetBa_2.1/Ion_Tracker.py
+++ DetBa_2.1/Ion_Tracker.py
@@ -159,18 +159,31 @@ class ION:
 		out_log	= str(outname) + "_TEMP.log"
 		Log	= open(out_log, 'w')
 		Log.write("frame\tdt\tfilename\tdirection (+/-)\n")
-		for file in file_list:
-			Data = open(file,'r')
-			for line in Data:
-				val = line.split()
-				if float(val[d_col]) > self.BsA_upper or float(val[d_col]) < self.BsB_lower:
+
+		for file in tqdm(file_list):
+        with open(file, "r") as f:
+            all_lines = f.read().splitlines()
+        # generate list of indexes denoting new ions
+        start_list = []
+        for i in range(0,len(all_lines),1):
+            if all_lines[i].split()[0] == "IonID:":
+                start_list.append(i)
+            else:
+                pass
+        if len(start_list) == 0:
+            start_list.append(0)
+        start_list.append(-1)
+        # Loop through each ion's index and process their data
+        for i in range(0,len(start_list)-1,1):
+            for line in all_lines[start_list[i]:start_list[i+1]:lag_step]:
+				if float(line.split()[d_col]) > self.BsA_upper or float(line.split()[d_col]) < self.BsB_lower:
 					pass
 				else:
-					bin_now 	= Which_Bin(float(val[d_col]))
-					Order_Assign(bin_now,int(val[0]))
-					check,direc,dt = Perm_Check(bin_now,int(val[0]))
+					bin_now 	= Which_Bin(float(line.split()[d_col]))
+					Order_Assign(bin_now,int(line.split()[0]))
+					check,direc,dt = Perm_Check(bin_now,int(line.split()[0]))
 					if check == True:
-						Log.write(str(val[0])+'\t'+str(dt)+'\t'+str(file)+'\t'+str(direc)+'\n')
+						Log.write(str(line.split()[0])+'\t'+str(dt)+'\t'+str(file)+'\t'+str(direc)+'\n')
 					else:
 						pass
 			RESET("MT")
@@ -204,6 +217,7 @@ def process(inname, lag_base):
 			permlist.append(hold)
 		# Now the data is in a more friendly plotable way.
 		# Time to just generate the linear models for the entire simulation, and the final 20 ns
+		# Create numpy arrays of your x & y values
 		perm = np.array(permlist)
 		time = np.array(timelist)
 		time = time.reshape(-1,1)
