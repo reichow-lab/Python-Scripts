a7688e4faaab89ef4fb3183e340415f7e0805ce5
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-01-04 00:05:40 -0800

    fix

--- DetBa_2.1/Ion_Tracker.py
+++ DetBa_2.1/Ion_Tracker.py
@@ -161,21 +161,21 @@ class ION:
 		Log.write("frame\tdt\tfilename\tdirection (+/-)\n")
 
 		for file in tqdm(file_list):
-        with open(file, "r") as f:
-            all_lines = f.read().splitlines()
+		with open(file, "r") as f:
+			all_lines = f.read().splitlines()
         # generate list of indexes denoting new ions
-        start_list = []
-        for i in range(0,len(all_lines),1):
-            if all_lines[i].split()[0] == "IonID:":
-                start_list.append(i)
-            else:
-                pass
-        if len(start_list) == 0:
-            start_list.append(0)
+		start_list = []
+		for i in range(0,len(all_lines),1):
+			if all_lines[i].split()[0] == "IonID:":
+				start_list.append(i)
+			else:
+				pass
+			if len(start_list) == 0:
+				start_list.append(0)
         start_list.append(-1)
         # Loop through each ion's index and process their data
-        for i in range(0,len(start_list)-1,1):
-            for line in all_lines[start_list[i]:start_list[i+1]:lag_step]:
+		for i in range(0,len(start_list)-1,1):
+			for line in all_lines[start_list[i]:start_list[i+1]:lag_step]:
 				if float(line.split()[d_col]) > self.BsA_upper or float(line.split()[d_col]) < self.BsB_lower:
 					pass
 				else:
