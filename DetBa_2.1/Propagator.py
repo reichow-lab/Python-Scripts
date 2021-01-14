151f1599d9142891ba07dae0f6cb1c663b351259
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-01-14 10:38:21 -0800

    fix

--- DetBa_2.1/Propagator.py
+++ DetBa_2.1/Propagator.py
@@ -111,17 +111,17 @@ def populate(file_list, pop_mat, bin_min, bin_max, bin_s, num_bins, array_dim, d
             for line in all_lines[start_list[i]:start_list[i+1]:lag_step]:
                 if bin_j == 'new':
                     if float(line.split()[d_col]) > bin_max:
-                        bin_j = bin_max
+                        bin_j = ZtoBin[bin_max]
                     elif float(line.split()[d_col]) < bin_min:
-                        bin_j = bin_min
+                        bin_j = ZtoBin[bin_min]
                     else:
                         bin_j = ZtoBin[int(float(line.split()[d_col]))]
                 else:
                     bin_i = bin_j
                     if float(line.split()[d_col]) > bin_max:
-                        bin_j = bin_max
+                        bin_j = ZtoBin[bin_max]
                     elif float(line.split()[d_col]) < bin_min:
-                        bin_j = bin_min
+                        bin_j = ZtoBin[bin_min]
                     else:
                         bin_j = ZtoBin[int(float(line.split()[d_col]))]
                     if array_dim == 1:    # Rates calculation: choice == 'R' or 'M'
