75324d766d9807c1efbbb92cf5f1de14cf338c92
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-01-14 10:07:08 -0800

    test flux through surface

--- DetBa_2.1/Calculator.py
+++ DetBa_2.1/Calculator.py
@@ -136,13 +136,12 @@ def check_SS(MSM,Pss,num_bins,lag_time,outname):
                 oflux += (Pss[state,0])*MSM[j,state]
 
             #flux through the i->j surface
-            ij_flux = 0
-            ji_flux = 0
-            j_list = list(range(state+1,num_bins))
-            for j in j_list:
-                ij_flux += Pss[state,0] * MSM[j,state]
+            ij_flux, ji_flux = 0, 0
+            for j in range(state+1,num_bins):
                 ji_flux += Pss[j,0] * MSM[state,j]
-            J_ij = ji_flux - ij_flux
+            for i in range(0,state):
+                ij_flux += Pss[i,0] * MSM[(state+1),i]
+            J_ij = ij_flux - ji_flux
             outss.write(f"{Pss[state][0]}\t{iflux/oflux}\t{J_ij}\n")
 
 
