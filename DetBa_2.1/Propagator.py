eaa6628440454865860473ad434abb5436c90749
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-01-14 19:15:17 -0800

    fix

--- DetBa_2.1/Propagator.py
+++ DetBa_2.1/Propagator.py
@@ -111,21 +111,13 @@ def populate(file_list, pop_mat, bin_min, bin_max, bin_s, num_bins, array_dim, d
         for i in range(len(start_list)-1):
             bin_j = 'new'
             for line in all_lines[start_list[i]:start_list[i+1]:lag_step]:
+                if abs(float(line.split()[d_col])) > bin_max:
+                    pass
                 if bin_j == 'new':
-                    if float(line.split()[d_col]) >= bin_max:
-                        bin_j = ZtoBin[bin_max]
-                    elif float(line.split()[d_col]) <= bin_min:
-                        bin_j = ZtoBin[bin_min]
-                    else:
-                        bin_j = ZtoBin[int(float(line.split()[d_col]))]
+                    bin_j = ZtoBin[int(float(line.split()[d_col]))]
                 else:
                     bin_i = bin_j
-                    if float(line.split()[d_col]) >= bin_max:
-                        bin_j = ZtoBin[bin_max]
-                    elif float(line.split()[d_col]) <= bin_min:
-                        bin_j = ZtoBin[bin_min]
-                    else:
-                        bin_j = ZtoBin[int(float(line.split()[d_col]))]
+                    bin_j = ZtoBin[int(float(line.split()[d_col]))]
                     if array_dim == 1:    # Rates calculation: choice == 'R' or 'M'
                         pop_mat[bin_j,bin_i] += 1
                     elif array_dim == 0:    # Histogram calculation: choice == 'H'
