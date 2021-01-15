91a50f687f4727ba741e13e3119d33c29d65b93e
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-01-14 19:25:12 -0800

    fix

--- DetBa_2.1/Propagator.py
+++ DetBa_2.1/Propagator.py
@@ -42,7 +42,7 @@ def initialize(file_list, bin_size, outname, array_dim, d_col, bin_lim='auto'):
                     bin_max = int(float(val[d_col]))
                 else:
                     pass
-        bin_dim = min(abs(bin_min),abs(bin_max)) - 5
+        bin_dim = min(abs(bin_min),abs(bin_max))
     else:
         bin_dim = int(bin_lim)
 
