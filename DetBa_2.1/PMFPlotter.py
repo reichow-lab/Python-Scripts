d8587d59322215b50764ff059c9b3ea69f0bdd69
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-01-29 09:40:40 -0800

    test

--- DetBa_2.1/PMFPlotter.py
+++ DetBa_2.1/PMFPlotter.py
@@ -53,8 +53,8 @@ for n in RateList:
     holdMin.append([])
     holdMax.append([])
 for x in range(len(MaxList[0])):
-    holdMin[MinList[x][1]].append(MinList[x][0])
-    holdMax[MaxList[x][1]].append(MaxList[x][0])
+    holdMin[int(MinList[x][1])].append(MinList[x][0])
+    holdMax[int(MaxList[x][1])].append(MaxList[x][0])
 for n in range(len(holdMin)):
     MinAvg.append(np.mean(holdMin[n]))
     MaxAvg.append(np.mean(holdMax[n]))
