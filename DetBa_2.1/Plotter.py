b965150b7c6b31ff6c7e47be578429c749d296ca
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-03-03 13:00:02 -0800

    new

---
+++ DetBa_2.1/Plotter.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from scipy.interpolate import interp1d
+import sys
+import argparse
+from PMFPlotter import PMFPlotter
+from TrackerPlot import TrackerPlot
+# Parse inputs
+parser = argparse.ArgumentParser()
+parser.add_argument("-dat", dest = "datstring", action = "store")
+parser.add_argument("-out", dest = "outname", action = "store", default = "OUTFILE")
+parser.add_argument("-p", "--pmf", dest = "Pchoice", action = "store", type=bool, default = False)
+parser.add_argument("-t", "--track", dest = "Tchoice", action = "store", type=bool, default = False)
+parser.add_argument("-b", "--obs" dest = "Bchoice", action = "store", type=bool, default = False)
+parser.add_argument("-bs", dest = "ObString", action = "store")
+parser.add_argument("-ws", "--windowsize", dest = "WS", action = "store")
+
+args = parser.parse_args()
+
+if args.Pchoice == True:
