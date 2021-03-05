5772c8039fd1cfce2ba1ec35f1201ad137597389
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-03-05 14:14:31 -0800

    test

--- DetBa_2.1/TrackerPlot.py
+++ DetBa_2.1/TrackerPlot.py
@@ -114,17 +114,16 @@ def TrackerPlot(system,start,outname,palette,WS,obs,LT,d_col,ObString):
             # Loop through each chain's index and process their data
             for i in range(1,len(start_list)):
                 for line in all_lines[start_list[i]:start_list[i+1]-1]:
-                    print(line.split()[0])
                     Semi[0].append(float(line.split()[0])/10)
                     Semi[i].append(float(line.split()[d_col]))
                 # Processdd the interpolated data
                 for ii in range(len(xnew)):
                     if i == 1:
-                        xnew,ynew = Interp(Semi[0],Semi[i])
+                        xnew,ynew = Interp(Semi[0],Semi[i],LT)
                         Obs[0].append(float(xnew[ii]))
                         Obs[i].append(int(ynew[ii]))
                     else:
-                        xnew,ynew = Interp(Semi[0],Semi[i])
+                        xnew,ynew = Interp(Semi[0],Semi[i],LT)
                         Obs[i].append(int(ynew[ii]))
         # Data is already in wide-format so no need for separating as we do above.
         for c in range(1,len(Obs)):
