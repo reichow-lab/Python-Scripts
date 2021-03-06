c5d2d257fa98057326066d06e17afe027270a212
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-03-05 17:57:47 -0800

    fix

--- DetBa_2.1/TrackerPlot.py
+++ DetBa_2.1/TrackerPlot.py
@@ -148,13 +148,12 @@ def TrackerPlot(system,start,outname,palette,WS,obs,LT,d_col,ObString):
     plot_data1 = pd.DataFrame({"Time (ns)": Final[0], "Ion Permeations": Final[1]})
     plot_data2 = pd.DataFrame({"Time (ns)": WinAvg[0], "current (pA)": WinAvg[1]})
     plot_data3 = pd.DataFrame({"Time (ns)": Final[0], "<current> (pA)": Final[5]})
-    plot_data4 = pd.DataFrame({"First-Passage Times (ns)": fptList[0]})
+    plot_data4 = pd.DataFrame({"Transition Times (ns)": fptList[0]})
     if bool(obs) == True:
         #print(len(WinAvg[1]),len(WinObs[1]))
         plot_data5 = pd.DataFrame({"Current (pA)": WinAvg[1]})
         for i in range(1,len(WinObs)):
-            print('debug')
-            plot_data5[str(i)] = WinObs[i]
+            plot_data5[str(i)] = pd.Series(WinObs[i])
         print(plot_data5)
         plt.title("Current vs. Observable")
         plt.xlabel("Current")
