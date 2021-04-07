7cc807a34deac71dc53100b8696d295155663a55
bassam <bhaddad@pdx.edu>
2021-04-06 18:09:39 -0700

    test

--- DetBa_2.1/Plotter.py
+++ DetBa_2.1/Plotter.py
@@ -101,7 +101,6 @@ def WatFluxTrack(system,outname,palette,WS,LT,d_col,watlim):
                 WindowAverage[0].append(WinAVG[0][0][i])
                 WindowAverage[1].append(WinAVG[z][1][i])
                 WindowAverage[2].append(zables[z])
-        print(CumPermeations[0])
         CumPermeations  = pd.DataFrame({"Time (ns)": CumPermeations[0], "Cumulative Permeations": CumPermeations[1], "Pore Height": CumPermeations[2]})
         CumAverage      = pd.DataFrame({"Time (ns)": CumAverage[0], "Cumulative Average": CumAverage[1], "Pore Height": CumAverage[2]})
         WindowAverage   = pd.DataFrame({"Time (ns)": WindowAverage[0], "Windowed Average Flux (ns^-1)": WindowAverage[1], "Pore Height": WindowAverage[2]})
