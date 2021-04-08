7ec8ac8fcacca71ecb969ceac53e082ecf37f49e
bassam <bhaddad@pdx.edu>
2021-04-07 18:47:21 -0700

    test

--- DetBa_2.1/Plotter.py
+++ DetBa_2.1/Plotter.py
@@ -165,6 +165,6 @@ if args.Wchoice == True:
         plt.xlabel("Time (ns)")
         ax = sns.lineplot(data=FinalDF,x="Time (ns)",y="Water Flux (ns^-1)",ax=ax,color="#00A6ED",label='Water')
         ax2 = sns.lineplot(data=FinalDF,x="Time (ns)",y="Ionic Current (pA)",ax=ax2,color="#F6511D",label='K+')
-        plt.legend()
+        ax.legend()
         plt.savefig(args.outname+"_WatVsCurr_line.png", dpi=400)
         plt.clf()
