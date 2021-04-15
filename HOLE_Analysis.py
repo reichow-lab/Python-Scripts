877109191f2963b4a3b6dc30b75dca295d370245
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-04-15 16:32:34 -0700

    test

--- HOLE_Analysis.py
+++ HOLE_Analysis.py
@@ -43,6 +43,6 @@ with open(str(globstring + '_Time.pkl'), 'wb') as out:
 
 plt.xlabel("Pore Axis")
 plt.ylabel('Pore Radii')
-sns.lineplot(data=Pore_Radii_Time palette=sns.color_palette(palette, n_colors=199))
+sns.lineplot(data=Pore_Radii_Time, palette=sns.color_palette('Blues_r', n_colors=199))
 plt.savefig(globstring+"_TEST.png", dpi=400)
 plt.clf()
