d4f18b9ce52a396bd57102974417a78ee0e340b0
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-04-15 15:57:04 -0700

    test

--- HOLE_Analysis.py
+++ HOLE_Analysis.py
@@ -11,23 +11,32 @@ hole_file_list = glob(globstring)
 hole_file_list.sort()
 
 # Load HOLE output files
-up_lim  = 64
-low_lim = -64
+up_lim  = 65
+low_lim = -65
 Pore_Radii = []
-Pore_Axis  = np.arange(-64,65)
-
+Pore_Axis  = np.arange(-65,66)
+Pore_Radii_Time = [[]]
+for i in range(-65,66):
+    Pore_Radii_Time[0].append(i)
+i = 0
 # Extract relevant data from HOLE output files
 for hole_file in hole_file_list:
+    i += 1
+    Pore_Radii_Time.append([])
     with open(hole_file) as FileIN:
         temp_radii = []
         for line in FileIN:
             val = line.split()
             if (float(val[0]) <= up_lim and float(val[0]) >= low_lim):
                 temp_radii.append(float(val[1]))
+                Pore_Radii_Time[i].append(float(val[1]))
         if len(temp_radii) > 0:
             Pore_Radii.append(temp_radii)
+
 
 # Save Extracted data for future processing
 with open(str(globstring + '_data.pkl'), 'wb') as out:
     pkl.dump(Pore_Radii, out)
     pkl.dump(Pore_Axis, out)
+with open(str(globstring + '_Time.pkl'), 'wb') as out:
+    pkl.dump(Pore_Radii_Time, out)
