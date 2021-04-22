b4d49b0ef6b287d576f203ab8abb91e190764063
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-04-22 12:01:00 -0700

    fixed

--- HOLE_Analysis.py
+++ HOLE_Analysis.py
@@ -18,7 +18,7 @@ args = parser.parse_args()
 
 # Create list of the appropriate HOLE files
 
-hole_file_list = glob(datstring)
+hole_file_list = glob(args.datstring)
 hole_file_list.sort()
 
 # Load HOLE output files
@@ -50,9 +50,9 @@ Pore_UpVsLow = [[],[],[]]
 for i in range(h):
     for j in range(len(Pore_Radii_Time[0])):
         # separate the two halves of the channel.
-        if Pore_Radii_Time[2] == i and Pore_Radii_Time[0] <= max and Pore_Radii_Time[0] >= min:
+        if Pore_Radii_Time[2] == i and Pore_Radii_Time[0] <= args.max and Pore_Radii_Time[0] >= args.min:
             hold_upper.append(Pore_Radii_Time[1])
-        elif Pore_Radii_Time[2] == i and Pore_Radii_Time[0] >= (-1*max) and Pore_Radii_Time[0] <= (-1*min):
+        elif Pore_Radii_Time[2] == i and Pore_Radii_Time[0] >= (-1*args.max) and Pore_Radii_Time[0] <= (-1*args.min):
             hold_lower.append(Pore_Radii_Time[1])
     Pore_UpVsLow[0].append(h*10)
     Pore_UpVsLow[1].append(np.mean(hold_upper))
@@ -68,9 +68,9 @@ PoreTimeDF  = pd.DataFrame({"Time (ns)": Pore_UpVsLow[0], "Upper Radii (Å)": Po
 plt.xlabel("Pore Axis")
 plt.ylabel('Pore Radii')
 sns.lineplot(data=PoreRadiiDF, x="Pore Axis", y="Pore Radii", hue=Pore_Radii_Time[2])
-plt.savefig(globstring+"_TTime.png", dpi=400)
+plt.savefig(args.outname+"_TTime.png", dpi=400)
 plt.clf()
 sns.lineplot(data=PoreTimeDF, x="Time (ns)", y="Upper Radii (Å)", palette=sns.color_palette(Reds_r, n_colors=1))
 sns.lineplot(data=PoreTimeDF, x="Time (ns)", y="Lower Radii (Å)", palette=sns.color_palette(Blues_r, n_colors=1))
-plt.savefig(globstring+"_RTime.png", dpi=400)
+plt.savefig(args.outname+"_RTime.png", dpi=400)
 plt.clf()
