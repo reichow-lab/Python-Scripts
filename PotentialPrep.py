4d1f9dfd3fb2e97f74c9c6fe9377294d99c1d503
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-05-17 16:18:02 -0700

    fix

--- PotentialPrep.py
+++ PotentialPrep.py
@@ -8,13 +8,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f",dest="infile",action="store")
 parser.add_argument("-e",dest="eField",type=float,action="store")
 parser.add_argument("-o",dest="outname",action="store")
+args = parser.parse_args()
 # Open the .dx from PMEPot, and calculate grid-demensions
-PMEPotIN = Grid(infile)
+PMEPotIN = Grid(args.infile)
 lenx, leny, lenz = PMEPot.grid.shape
 # loop through the z-axis of the reaction potential and add the applied linear-potential
 for z in range(lenz):
     for x in range(lenx):
         for y in range(leny):
-            PMEPotIN.grid[x][y][z] = PMEPotIN.grid[x][y][z] + (z*eField)
+            PMEPotIN.grid[x][y][z] = PMEPotIN.grid[x][y][z] + (z*args.eField)
 # writeout new potential grid
-PMEPotIN.export(outname+'.dx')
+PMEPotIN.export(args.outname+'.dx')
